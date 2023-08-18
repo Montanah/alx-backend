ce34d5b3006bdd8d8478b4cf10235988428471bf
Montanah <mititi.isaac@gmail.com>
2023-08-18 22:26:02 +0000

    Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,4 +39,6 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
         self.dataset()
         start, end = index_range(page, page_size)
+        if start_index >= len(dataset):
+            return []
         return list(range(start, end))
