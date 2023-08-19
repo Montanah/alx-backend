7b9294f90ebbd7491933d210153e9d0ac3ada00b
Montanah <mititi.isaac@gmail.com>
2023-08-19 09:33:50 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -39,7 +39,7 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
         dataset = self.dataset()
         start, end = index_range(page, page_size)
-        if start_index >= len(dataset):
+        if start >= len(dataset):
             return []
         return dataset[start: end]
 
