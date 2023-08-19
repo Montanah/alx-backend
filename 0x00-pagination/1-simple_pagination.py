854629f322f8be46c671229510fefc5582bab435
Montanah <mititi.isaac@gmail.com>
2023-08-19 09:27:59 +0000

    csv file setup

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,6 +39,6 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
         dataset = self.dataset()
         start, end = index_range(page, page_size)
-        if start_index >= len(dataset):
+        if start >= len(dataset):
             return []
         return dataset[start: end]
