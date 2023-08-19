02cc0d9ea5cf7114bd4c6020593090a337e8e823
Montanah <mititi.isaac@gmail.com>
2023-08-19 09:46:07 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -45,23 +45,23 @@ class Server:
 
 
      def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """Return a dictionary containing the following key-value pairs:
-        - page_size: the length of the returned dataset page
-        - page: the current page number
-        - data: the dataset page (equivalent to return from previous task)
-        - next_page: number of the next page, None if no next page
-        - prev_page: number of the previous page, None if no previous page
-        - total_pages: the total number of pages in the dataset as an integer
-        """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        dataset = self.dataset()
-        total_pages = math.ceil(len(self.__dataset) / page_size)
-        return {
-            'page_size': page_size if page < total_pages else 0,
-            'page': page,
-            'data': self.get_page(page, page_size),
-            'next_page': page + 1 if page < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages
+         """Return a dictionary containing the following key-value pairs:
+         - page_size: the length of the returned dataset page
+         - page: the current page number
+         - data: the dataset page (equivalent to return from previous task)
+         - next_page: number of the next page, None if no next page
+         - prev_page: number of the previous page, None if no previous page
+         - total_pages: the total number of pages in the dataset as an integer
+         """
+         assert isinstance(page, int) and page > 0
+         assert isinstance(page_size, int) and page_size > 0
+         dataset = self.dataset()
+         total_pages = math.ceil(len(self.__dataset) / page_size)
+         return {
+             'page_size': page_size if page < total_pages else 0,
+             'page': page,
+             'data': self.get_page(page, page_size),
+             'next_page': page + 1 if page < total_pages else None,
+             'prev_page': page - 1 if page > 1 else None,
+             'total_pages': total_pages
         }
