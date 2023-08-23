719665782299bc3775c69d1a19daa68c4444931e
Montanah <mititi.isaac@gmail.com>
2023-08-23 22:44:12 +0000

    LFU caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -11,42 +11,30 @@ class LFUCache(BaseCaching):
         """ Constructor """
         super().__init__()
         self.frequency = {}
-        self.frequency_list = {}
 
-    def update_frequency_list(self, key):
-        """ Update frequency list for the given key """
-        freq = self.frequency[key]
-        self.frequency_list[freq].remove(key)
-        if not self.frequency_list[freq]:
-            del self.frequency_list[freq]
-
-        freq += 1
-        self.frequency[key] = freq
-        if freq not in self.frequency_list:
-            self.frequency_list[freq] = []
-        self.frequency_list[freq].append(key)
+    def update_frequency(self, key):
+        """ Update frequency of the given key """
+        self.frequency[key] += 1
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key and item:
             if key in self.cache_data:
-                self.cache_data[key] = item
-                self.update_frequency_list(key)
+                self.update_frequency(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                min_freq_key = min(self.frequency, key=self.frequency.get)
+                del self.cache_data[min_freq_key]
+                del self.frequency[min_freq_key]
+                print("DISCARD: {}".format(min_freq_key))
+            self.cache_data[key] = item
+            if key in self.frequency:
+                self.update_frequency(key)
             else:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    min_freq = min(self.frequency_list)
-                    remove_key = self.frequency_list[min_freq].pop(0)
-                    del self.cache_data[remove_key]
-                    del self.frequency[remove_key]
-                self.cache_data[key] = item
                 self.frequency[key] = 1
-                if 1 not in self.frequency_list:
-                    self.frequency_list[1] = []
-                self.frequency_list[1].append(key)
 
     def get(self, key):
         """ Get an item by key """
         if key and key in self.cache_data:
-            self.update_frequency_list(key)
+            self.update_frequency(key)
             return self.cache_data[key]
         return None
