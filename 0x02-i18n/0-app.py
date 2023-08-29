630c3db5dad548044417f11d467b39c52f005e17
Montanah <mititi.isaac@gmail.com>
2023-08-29 21:22:55 +0000

    Basic Flask App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world() -> str:
+def index():
     """ 
     Return:
       - welcome message
     """
-    return render_template('0-index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
