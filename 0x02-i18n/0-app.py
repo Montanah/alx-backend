3ba18a2e4319e9fea4955466a9f991aabff3fece
Montanah <mititi.isaac@gmail.com>
2023-08-29 21:26:41 +0000

    Basic Flask App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,7 +12,7 @@ def index():
     Return:
       - welcome message
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
