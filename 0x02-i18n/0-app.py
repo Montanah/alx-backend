5d9dcfea6c57125cbe4cdf6baa154cee27e32bb0
Montanah <mititi.isaac@gmail.com>
2023-08-29 21:07:24 +0000

    Basic Flask App

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
 
-from flask import Flask, jsonify, request, abort, redirect
-from auth import Auth
-
-AUTH = Auth()
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/')
 def hello_world() -> str:
-    """ GET /
+    """ 
     Return:
       - welcome message
     """
-    return jsonify({"message": "Bienvenue"})
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+   app.run(debug=True)
