17e55fde00b20fbff12662f3f4592754b9c4977c
Montanah <mititi.isaac@gmail.com>
2023-08-30 11:48:16 +0000

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -17,10 +17,12 @@ class Config(object):
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale():
     """ GET locale from request URL """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
