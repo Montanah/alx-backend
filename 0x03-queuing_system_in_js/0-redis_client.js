3f00c92b8db20f19d133d3faf9e4553d17b13068
Montanah <mititi.isaac@gmail.com>
2023-09-27 14:43:32 +0000

    Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,20 @@
+/* Using Babel and ES6, write a script that should connect to the Redis server running on your machine:
+It should log to the console the message Redis client connected to the server when the connection to Redis works correctly
+It should log to the console the message Redis client not connected to the server: ERROR_MESSAGE when the connection to Redis does not work
+To import the library, you need to use the keyword import
+*/
+
+import redis from 'redis';
+
+const client = redis.createClient({
+  host: '127.0.0.1',
+  port: 6379,
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
